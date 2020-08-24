8cec4d7cb436d94d0fd2e904dc601636b1aa72a3
sscottgvit <sscottgvit>
2020-08-24 11:56:44 -0500

    Revise tests

--- setup.py
+++ setup.py
@@ -2,25 +2,22 @@ from distutils.core import setup
 
 setup(
     name='django-auditlog',
-    version='0.4.5',
+    version='0.4.7',
     packages=['auditlog', 'auditlog.migrations', 'auditlog.management', 'auditlog.management.commands'],
     package_dir={'': 'src'},
-    url='https://github.com/jjkester/django-auditlog',
+    url='https://github.com/GoVanguard/django-auditlog',
     license='MIT',
-    author='Jan-Jelle Kester',
+    author='Shane William Scott; Jan-Jelle Kester',
     description='Audit log app for Django',
     install_requires=[
         'django-jsonfield>=1.0.0',
-        'python-dateutil==2.6.0'
+        'python-dateutil>=2.6.0'
     ],
     zip_safe=False,
     classifiers=[
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
         'License :: OSI Approved :: MIT License',
     ],        
 )
